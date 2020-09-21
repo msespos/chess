0861d40631d93ee814d40f570bc6a64102c8f8bd
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-21 08:44:41 -0700

    Revert "Add initialize and (verbose) board methods"
    This reverts commit 24ce2cfbce3f6dcd336e93e2f121b6c8a2fdbc37.

--- lib/board.rb
+++ lib/board.rb
@@ -2,20 +2,4 @@
 
 # board class
 class Board
-  def initialize
-    board
-  end
-
-  # build a board with opening setup
-  def board
-    @board = []
-    @board.push(%i[WR WN WB WQ WK WB WN WR])
-    @board.push(%i[WP WP WP WP WP WP WP WP])
-    @board.push([nil, nil, nil, nil, nil, nil, nil, nil])
-    @board.push([nil, nil, nil, nil, nil, nil, nil, nil])
-    @board.push([nil, nil, nil, nil, nil, nil, nil, nil])
-    @board.push([nil, nil, nil, nil, nil, nil, nil, nil])
-    @board.push(%i[BP BP BP BP BP BP BP BP])
-    @board.push(%i[BR BN BB BQ BK BB BN BR])
-  end
 end
