72a03012186972f24b7506c10d192b2adeb41aa6
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-22 11:41:11 -0700

    Rename Board constants and add spacing to Board#board and update Board#to_s

--- lib/board.rb
+++ lib/board.rb
@@ -14,8 +14,9 @@ class Board
   B_ROOK = " \u265C".encode('utf-8')
   B_QUEEN = " \u265B".encode('utf-8')
   B_KING = " \u265A".encode('utf-8')
-  EMPTY = ' -'
-  BLANK = '  '
+  EMPTY_SQUARE = ' -'
+  BLANK_SPOT = '  '
+  BOARD_HEIGHT = 14
 
   def initialize
     opening_board
@@ -23,27 +24,31 @@ class Board
 
   # build a board with opening setup
   def opening_board
-    @board = Array.new(10) { Array.new(10) { EMPTY } }
-    @board[0] = [BLANK, ' a', ' b', ' c', ' d', ' e', ' f', ' g', ' h', BLANK]
-    @board[1] = [' 1', W_ROOK, W_KNIGHT, W_BISHOP, W_QUEEN, W_KING, W_BISHOP, W_KNIGHT, W_ROOK, ' 1']
-    @board[2] = [' 2', W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, ' 2']
-    @board[3][0] = ' 3'
-    @board[3][9] = ' 3'
-    @board[4][0] = ' 4'
-    @board[4][9] = ' 4'
-    @board[5][0] = ' 5'
-    @board[5][9] = ' 5'
-    @board[6][0] = ' 6'
-    @board[6][9] = ' 6'
-    @board[7] = [' 7', B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, ' 7']
-    @board[8] = [' 8', B_ROOK, B_KNIGHT, B_BISHOP, B_QUEEN, B_KING, B_BISHOP, B_KNIGHT, B_ROOK, ' 8']
-    @board[9] = [BLANK, ' a', ' b', ' c', ' d', ' e', ' f', ' g', ' h', BLANK]
+    @board = Array.new(12) { Array.new(10) { EMPTY_SQUARE } }
+    @board[0] = Array.new(10) { BLANK_SPOT }
+    @board[1] = [BLANK_SPOT, '   a', ' b', ' c', ' d', ' e', ' f', ' g', ' h', BLANK_SPOT]
+    @board[2] = Array.new(10) { BLANK_SPOT }
+    @board[3] = [' 1  ', W_ROOK, W_KNIGHT, W_BISHOP, W_QUEEN, W_KING, W_BISHOP, W_KNIGHT, W_ROOK, '   1']
+    @board[4] = [' 2  ', W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, W_PAWN, '   2']
+    @board[5][0] = ' 3  '
+    @board[5][9] = '   3'
+    @board[6][0] = ' 4  '
+    @board[6][9] = '   4'
+    @board[7][0] = ' 5  '
+    @board[7][9] = '   5'
+    @board[8][0] = ' 6  '
+    @board[8][9] = '   6'
+    @board[9] = [' 7  ', B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, B_PAWN, '   7']
+    @board[10] = [' 8  ', B_ROOK, B_KNIGHT, B_BISHOP, B_QUEEN, B_KING, B_BISHOP, B_KNIGHT, B_ROOK, '   8']
+    @board[11] = Array.new(10) { BLANK_SPOT }
+    @board[12] = [BLANK_SPOT, '   a', ' b', ' c', ' d', ' e', ' f', ' g', ' h', BLANK_SPOT]
+    @board[13] = Array.new(10) { BLANK_SPOT }
   end
 
   def to_s
     string = ''
-    (0..9).each do |row|
-      (0..9).each { |col| string += @board[9 - row][col].to_s }
+    (0..BOARD_HEIGHT - 1).each do |row|
+      (0..BOARD_HEIGHT - 1).each { |col| string += @board[BOARD_HEIGHT - 1 - row][col].to_s }
       string += "\n"
     end
     string
