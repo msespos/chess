e7879bba305dd3069b66392f55169872987593e3
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-22 21:59:16 -0700

    Rewrite tests for Board#opening_board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,37 +8,37 @@ RSpec.describe Board do
   subject(:board) { described_class.new }
   describe '#initialize' do
     context 'when the board class is instantiated' do
-      it 'calls the #board method' do
-        expect(board).to receive(:board)
+      it 'calls #opening_board' do
+        expect(board).to receive(:opening_board)
         board.send(:initialize)
       end
     end
   end
 
-  describe '#board' do
-    context 'when #board is called' do
-      it 'generates an 8 row board' do
-        expect(board.instance_variable_get(:@board)[0].size).to eq(8)
+  describe '#opening_board' do
+    context 'when #opening_board is called' do
+      it 'generates a 14 row board' do
+        expect(board.instance_variable_get(:@board).size).to eq(14)
       end
 
-      it 'generates an 8 column board' do
-        expect(board.instance_variable_get(:@board).size).to eq(8)
+      it 'generates a 10 column board' do
+        expect(board.instance_variable_get(:@board)[0].size).to eq(10)
       end
 
       it 'generates a board with WR in a1' do
-        expect(board.instance_variable_get(:@board)[0][0]).to eq(" \u2656".encode('utf-8'))
+        expect(board.instance_variable_get(:@board)[3][1]).to eq(" \u2656".encode('utf-8'))
       end
 
       it 'generates a board with BR in h8' do
-        expect(board.instance_variable_get(:@board)[7][7]).to eq(" \u265C".encode('utf-8'))
+        expect(board.instance_variable_get(:@board)[10][8]).to eq(" \u265C".encode('utf-8'))
       end
 
-      it 'generates a board with nil in a4' do
-        expect(board.instance_variable_get(:@board)[3][0]).to eq(nil)
+      it 'generates a board with " -" in a4' do
+        expect(board.instance_variable_get(:@board)[6][1]).to eq(' -')
       end
 
-      it 'generates a board with nil in c5' do
-        expect(board.instance_variable_get(:@board)[4][2]).to eq(nil)
+      it 'generates a board with " -" in c5' do
+        expect(board.instance_variable_get(:@board)[7][5]).to eq(' -')
       end
     end
   end
