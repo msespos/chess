85af12f444e3374419769ecffbda56f0a05e802b
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-25 21:08:25 -0700

    Rename Game#opening_playing_field to Game#starting_playing_field and add test for Game#starting_playing_field

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,8 +5,8 @@
 require_relative '../lib/game.rb'
 
 RSpec.describe Game do
+  subject(:game) { described_class.new }
   describe '#initialize' do
-    subject(:game) { described_class.new }
     context 'when the game class is instantiated' do
       it 'creates an instance of Board' do
         board = game.instance_variable_get(:@board)
@@ -14,9 +14,26 @@ RSpec.describe Game do
         game.send(:initialize)
       end
 
-      it 'calls #opening_playing_field' do
-        expect(game).to receive(:opening_playing_field)
+      it 'calls #starting_playing_field' do
+        expect(game).to receive(:starting_playing_field)
         game.send(:initialize)
+      end
+    end
+  end
+
+  describe '#starting_playing_field' do
+    context 'when playing field is created' do
+      it 'has the pieces and empty squares set up correctly' do
+        field = game.instance_variable_get(:@playing_field)
+        expect(field).to eq([%i[w_rook w_knight w_bishop w_queen w_king w_bishop w_knight w_rook],
+                             %i[w_pawn w_pawn w_pawn w_pawn w_pawn w_pawn w_pawn w_pawn],
+                             [nil, nil, nil, nil, nil, nil, nil, nil],
+                             [nil, nil, nil, nil, nil, nil, nil, nil],
+                             [nil, nil, nil, nil, nil, nil, nil, nil],
+                             [nil, nil, nil, nil, nil, nil, nil, nil],
+                             %i[b_pawn b_pawn b_pawn b_pawn b_pawn b_pawn b_pawn b_pawn],
+                             %i[b_rook b_knight b_bishop b_queen b_king b_bishop b_knight b_rook]])
+        game.starting_playing_field
       end
     end
   end
