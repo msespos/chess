3eaeebef28e971c49834cce27678f5aae3b80020
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-26 07:43:28 -0700

    Add comments on two methods

--- lib/game.rb
+++ lib/game.rb
@@ -7,6 +7,7 @@ class Game
     starting_playing_field
   end
 
+  # set up the playing field for the start of the game
   def starting_playing_field
     @playing_field = Array.new(8) { Array.new(8) { nil } }
     @playing_field[0] = %i[w_rook w_knight w_bishop w_queen w_king w_bishop w_knight w_rook]
@@ -15,6 +16,7 @@ class Game
     @playing_field[7] = %i[b_rook b_knight b_bishop b_queen b_king b_bishop b_knight b_rook]
   end
 
+  # convert an algebraic expression (user input) to cartesian coordinates (for piece manipulation)
   def algebraic_to_cartesian(algebraic_expression)
     [algebraic_expression.ord - 97, algebraic_expression[1].to_i - 1]
   end
