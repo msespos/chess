bee24c5008bd7077eab30e7d36eaead90d262b2b
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-25 18:29:07 -0700

    Add tests for Game#initialize

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,4 +4,22 @@
 
 require_relative '../lib/game.rb'
 
+RSpec.describe Game do
+  describe '#initialize' do
+    subject(:game) { described_class.new }
+    context 'when the game class is instantiated' do
+      it 'creates an instance of Board' do
+        board = game.instance_variable_get(:@board)
+        expect(board).to be_a(Board)
+        game.send(:initialize)
+      end
+
+      it 'calls #opening_playing_field' do
+        expect(game).to receive(:opening_playing_field)
+        game.send(:initialize)
+      end
+    end
+  end
+end
+
 # rubocop:enable Metrics/BlockLength
