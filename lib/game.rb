56ae390d93a7fe03a0ff98f1d40406eb333a3c13
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-27 09:03:44 -0700

    Refactor multiple methods in Game to replace start[0] to start0 and related

--- lib/game.rb
+++ lib/game.rb
@@ -23,37 +23,46 @@ class Game
     [algebraic_expression.ord - 97, algebraic_expression[1].to_i - 1]
   end
 
+  def start_finish_to_variables(start, finish)
+    start0 = start[0]
+    start1 = start[1]
+    finish0 = finish[0]
+    finish1 = finish[1]
+    [start0, start1, finish0, finish1]
+  end
+
   # move a piece (capturing or not) given start and finish coordinates
   # castling and en passant and pawn promotion still need to be incorporated
   def move_piece(start, finish)
-    return :invalid unless valid_move?(start, finish)
+    start0, start1, finish0, finish1 = start_finish_to_variables(start, finish)
+    return :invalid unless valid_move?(start0, start1, finish0, finish1)
 
-    temp = @playing_field[start[0]][start[1]]
-    @playing_field[start[0]][start[1]] = nil
-    captured = capture?(finish) ? @playing_field[finish[0]][finish[1]] : nil
-    @playing_field[finish[0]][finish[1]] = temp
+    temp = @playing_field[start0][start1]
+    @playing_field[start0][start1] = nil
+    captured = capture?(finish0, finish1) ? @playing_field[finish0][finish1] : nil
+    @playing_field[finish0][finish1] = temp
     captured
   end
 
-  # checks if the move is valid by calling path_to? on the piece in the start square
+  # check if the move is valid by calling path_to? on the piece in the start square
   # and the start and finish coordinates
-  def valid_move?(start, finish)
-    return false if same_color?(@playing_field[start[0]][start[1]], @playing_field[finish[0]][finish[1]])
+  def valid_move?(start0, start1, finish0, finish1)
+    return false if same_color?(@playing_field[start0][start1], @playing_field[finish0][finish1])
 
-    path_to?(@playing_field[start[0]][start[1]], start, finish)
+    path_to?(start0, start1, finish0, finish1)
   end
 
-  # checks if there is a capture in the move by checking if there is a piece in the finish square
-  def capture?(finish)
-    !@playing_field[finish[0]][finish[1]].nil?
+  # check if there is a capture in the move by checking if there is a piece in the finish square
+  def capture?(finish0, finish1)
+    !@playing_field[finish0][finish1].nil?
   end
 
-  # checks if the pieces in the start and finish square are the same color or not
-  def same_color?(start, finish)
-    @playing_field[start[0]][start[1]][0] == @playing_field[finish[0]][finish[1]][0]
+  # check if the pieces in the start and finish square are the same color or not
+  def same_color?(start_piece, finish_piece)
+    start_piece[0] == finish_piece[0]
   end
 
   # determines if there is a path from the start to the finish using the piece, the start and finish
   # coordinates, and the current state of the playing field
-  def path_to?(piece, start, finish) end
+  def path_to?(start0, start1, finish0, finish1) end
 end
