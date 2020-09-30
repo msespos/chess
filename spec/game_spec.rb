959aa8c3684c7a57c0519de197bc77f70502a848
Mike Esposito <mikeesposito6852@gmail.com>
2020-09-30 08:52:37 -0700

    Modify tests to reflect new structure of #move_piece and related methods in Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -152,7 +152,7 @@ RSpec.describe Game do
     context 'when the pieces are the same color' do
       it 'returns false' do
         allow(game).to receive(:same_color?).and_return(true)
-        expect(game.valid_move?(0, 0, 0, 1)).to eq(false)
+        expect(game.valid_move?([0, 0], [0, 1])).to eq(false)
       end
     end
 
@@ -163,7 +163,7 @@ RSpec.describe Game do
         game.instance_variable_set(:@piece, piece_valid)
         allow(game).to receive(:same_color?).and_return(false)
         allow(piece_valid).to receive(:rook_path?).and_return(false)
-        expect(game.valid_move?(0, 0, 0, 1)).to eq(false)
+        expect(game.valid_move?([0, 0], [0, 1])).to eq(false)
       end
     end
 
@@ -174,7 +174,7 @@ RSpec.describe Game do
         game.instance_variable_set(:@piece, piece_valid)
         allow(game).to receive(:same_color?).and_return(false)
         allow(piece_valid).to receive(:rook_path?).and_return(true)
-        expect(game.valid_move?(0, 0, 0, 1)).to eq(true)
+        expect(game.valid_move?([0, 0], [0, 1])).to eq(true)
       end
     end
   end
@@ -183,14 +183,14 @@ RSpec.describe Game do
     context 'when the finish square is empty' do
       it 'returns false' do
         game.instance_variable_get(:@playing_field)[7][5] = nil
-        expect(game.capture?(7, 5)).to eq(false)
+        expect(game.capture?([7, 5])).to eq(false)
       end
     end
 
     context 'when the finish square has a piece on it' do
       it 'returns true' do
         game.instance_variable_get(:@playing_field)[7][5] = :w_rook
-        expect(game.capture?(7, 5)).to eq(true)
+        expect(game.capture?([7, 5])).to eq(true)
       end
     end
   end
