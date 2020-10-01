03583dd79af970d1e98137c79cfc0c426376fc8d
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-01 13:37:59 -0700

    Add bishop.rb and Bishop class and Bishop#path? function to be called by Piece

---
+++ lib/bishop.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# rook class
+class Bishop < Piece
+  # determine if a path is legal for a bishop using the start, finish and playing field
+  # uses functions in Piece
+  def path?(start, finish, playing_field)
+    return false unless along_positive_diagonal?(start, finish) || along_negative_diagonal?(start, finish)
+
+    return positive_diagonal_free?(start, finish, playing_field) if along_positive_diagonal?(start, finish)
+    return negative_diagonal_free?(start, finish, playing_field) if along_negative_diagonal?(start, finish)
+  end
+end
