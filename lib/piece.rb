dd3f52d14d9f9503814d55f6cdc38d08f8362b44
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-01 08:26:44 -0700

    Refactor Piece#rook_path? to call Rook#path?

--- lib/piece.rb
+++ lib/piece.rb
@@ -5,10 +5,8 @@ class Piece
   # determines if a path is legal for a rook using the start, finish and playing field
   # uses #along_rank?, #along_file?, #rank_free? and #file_free?
   def rook_path?(start, finish, playing_field)
-    return false unless along_rank?(start, finish) || along_file?(start, finish)
-
-    return rank_free?(start, finish, playing_field) if along_rank?(start, finish)
-    return file_free?(start, finish, playing_field) if along_file?(start, finish)
+    @rook = Rook.new
+    @rook.path?(start, finish, playing_field)
   end
 
   # used by #rook_path? and queen_path? to determine if all spots on a rank
