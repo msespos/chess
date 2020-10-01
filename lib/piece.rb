5e3340166ba9674f44c6911c43912d9253d01d64
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-01 11:45:05 -0700

    Refactor #rank_free? and file_free? and add helper methods to fix bug in which counter was not counting correctly from finish to start half the time

--- lib/piece.rb
+++ lib/piece.rb
@@ -9,30 +9,48 @@ class Piece
     @rook.path?(start, finish, playing_field)
   end
 
-  # used by #rook_path? and queen_path? to determine if all spots on a rank
+  # used by Rook#path? and Queen#path? to determine if all spots on a rank
   # between the start and finish are free
   def rank_free?(start, finish, playing_field)
-    (start[0] + 1..finish[0] - 1).each do |file|
-      return false unless playing_field[file][start[0]].nil?
+    if start[0] < finish[0]
+      blank_rank_path?(start[0], finish[0], start, playing_field)
+    else
+      blank_rank_path?(finish[0], start[0], start, playing_field)
+    end
+  end
+
+  # used by #rank_free to check that the inspected spaces are free
+  def blank_rank_path?(left, right, start, playing_field)
+    (left + 1..right - 1).each do |file|
+      return false unless playing_field[file][start[1]].nil?
     end
     true
   end
 
-  # used by #rook_path? and queen_path? to determine if all spots on a rank
+  # used by #Rook#path? and Queen#path? to determine if all spaces on a rank
   # between the start and finish are free
   def file_free?(start, finish, playing_field)
-    (start[1] + 1..finish[1] - 1).each do |rank|
+    if start[1] < finish[1]
+      blank_file_path?(start[1], finish[1], start, playing_field)
+    else
+      blank_file_path?(finish[1], start[1], start, playing_field)
+    end
+  end
+
+  # used by #file_free to check that the inspected spaces are free
+  def blank_file_path?(bottom, top, start, playing_field)
+    (bottom + 1..top - 1).each do |rank|
       return false unless playing_field[start[1]][rank].nil?
     end
     true
   end
 
-  # used by #rook_path? and #queen_path? to determine if the potential path is along a rank
+  # used by #Rook#path? and #Queen#path? to determine if the potential path is along a rank
   def along_rank?(start, finish)
     start[1] == finish[1]
   end
 
-  # used by #rook_path? and #queen_path? to determine if the potential path is along a file
+  # used by #Rook#path? and #Queen#path? to determine if the potential path is along a file
   def along_file?(start, finish)
     start[0] == finish[0]
   end
