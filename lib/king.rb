6bb0715077b70afa17a64909b87474f77f362fa2
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-03 15:29:35 -0700

    Remove #only_one_space? and #castling? and refactor #path?

--- lib/king.rb
+++ lib/king.rb
@@ -4,16 +4,8 @@ require_relative 'piece.rb'
 
 # king class
 class King < Piece
-  # determine if a path is legal for a king using the start, finish and playing field
-  def path?(start, finish, playing_field)
-    return false unless only_one_space?(start, finish) || castling?(start, finish, playing_field)
-
-    true
-  end
-
-  def only_one_space?(start, finish)
+  # determine if a path is legal for a king using the start and finish (playing field not used)
+  def path?(start, finish)
     (start[0] - finish[0]).abs <= 1 && (start[1] - finish[1]).abs <= 1
   end
-
-  def castling?(start, finish, playing_field) end
 end
