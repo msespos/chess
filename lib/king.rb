df0124cac66b34f7f054bedf1a14b6cad815a03e
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-03 14:36:08 -0700

    Refactor #only_one_space? for more brevity and remove incorrect comment

--- lib/king.rb
+++ lib/king.rb
@@ -5,7 +5,6 @@ require_relative 'piece.rb'
 # king class
 class King < Piece
   # determine if a path is legal for a king using the start, finish and playing field
-  # uses methods in Piece
   def path?(start, finish, playing_field)
     return false unless only_one_space?(start, finish) || castling?(start, finish, playing_field)
 
@@ -13,8 +12,7 @@ class King < Piece
   end
 
   def only_one_space?(start, finish)
-    start[0] - finish[0] <= 1 && start[0] - finish[0] >= -1 &&
-      start[1] - finish[1] <= 1 && start[1] - finish[1] >= -1
+    (start[0] - finish[0]).abs <= 1 && (start[1] - finish[1]).abs <= 1
   end
 
   def castling?(start, finish, playing_field) end
