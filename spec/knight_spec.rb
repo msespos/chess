7dfa3e31243cd400a2a0260342df64fa42308a94
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-03 17:37:51 -0700

    Rewrite tests as one #path? test and multiple #vertical_l? and #horizontal_l? tests

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -7,57 +7,72 @@ require_relative '../lib/knight.rb'
 RSpec.describe Knight do
   subject(:knight) { described_class.new }
   describe '#path?' do
-    context 'when the start and finish are not a knight"s move apart' do
-      it 'returns false' do
-        path_or_not = knight.path?([4, 0], [6, 0])
-        expect(path_or_not).to eq(false)
+    context 'when the start and finish are not an L apart from each other' do
+      it 'returns true' do
+        allow(knight).to receive(:vertical_l?).and_return(false)
+        allow(knight).to receive(:horizontal_l?).and_return(false)
+        expect(knight).to receive(:path?).and_return(false)
+        knight.path?('start', 'finish')
+      end
+    end
+  end
+
+  describe 'vertical_l?' do
+    context 'when the start and finish are a vertical L apart' do
+      it 'returns true' do
+        vertical_l_or_not = knight.vertical_l?([4, 0], [5, 2])
+        expect(vertical_l_or_not).to eq(true)
       end
 
-      it 'returns false' do
-        path_or_not = knight.path?([4, 0], [4, 5])
-        expect(path_or_not).to eq(false)
+      it 'returns true' do
+        vertical_l_or_not = knight.vertical_l?([4, 5], [5, 3])
+        expect(vertical_l_or_not).to eq(true)
       end
 
-      it 'returns false' do
-        path_or_not = knight.path?([1, 1], [5, 5])
-        expect(path_or_not).to eq(false)
+      it 'returns true' do
+        vertical_l_or_not = knight.vertical_l?([2, 3], [1, 1])
+        expect(vertical_l_or_not).to eq(true)
       end
 
-      it 'returns false' do
-        path_or_not = knight.path?([1, 2], [7, 8])
-        expect(path_or_not).to eq(false)
+      it 'returns true' do
+        vertical_l_or_not = knight.vertical_l?([5, 6], [4, 8])
+        expect(vertical_l_or_not).to eq(true)
       end
     end
 
-    context 'when the start and finish are a knight"s move apart' do
+    context 'when the start and finish are not a vertical L apart' do
+      it 'returns false' do
+        vertical_l_or_not = knight.vertical_l?([0, 0], [5, 2])
+        expect(vertical_l_or_not).to eq(false)
+      end
+    end
+
+    context 'when the start and finish are a horizontal L apart' do
       it 'returns true' do
-        path_or_not = knight.path?([4, 0], [5, 2])
-        expect(path_or_not).to eq(true)
+        horizontal_l_or_not = knight.horizontal_l?([4, 0], [2, 1])
+        expect(horizontal_l_or_not).to eq(true)
       end
 
       it 'returns true' do
-        path_or_not = knight.path?([4, 0], [3, 2])
-        expect(path_or_not).to eq(true)
+        horizontal_l_or_not = knight.horizontal_l?([7, 7], [5, 6])
+        expect(horizontal_l_or_not).to eq(true)
       end
 
       it 'returns true' do
-        path_or_not = knight.path?([7, 7], [6, 5])
-        expect(path_or_not).to eq(true)
+        horizontal_l_or_not = knight.horizontal_l?([0, 2], [2, 3])
+        expect(horizontal_l_or_not).to eq(true)
       end
 
       it 'returns true' do
-        path_or_not = knight.path?([7, 2], [5, 3])
-        expect(path_or_not).to eq(true)
+        horizontal_l_or_not = knight.horizontal_l?([6, 2], [8, 1])
+        expect(horizontal_l_or_not).to eq(true)
       end
+    end
 
-      it 'returns true' do
-        path_or_not = knight.path?([6, 2], [8, 1])
-        expect(path_or_not).to eq(true)
-      end
-
-      it 'returns true' do
-        path_or_not = knight.path?([4, 4], [6, 3])
-        expect(path_or_not).to eq(true)
+    context 'when the start and finish are not a horizontal L apart' do
+      it 'returns false' do
+        horizontal_l_or_not = knight.horizontal_l?([4, 4], [6, 6])
+        expect(horizontal_l_or_not).to eq(false)
       end
     end
   end
