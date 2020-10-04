f4f7d7a4c5b20a82c08b557fa89c9e33892777b8
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-04 11:08:35 -0700

    Add pawn_spec.rb

---
+++ spec/pawn_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require_relative '../lib/pawn.rb'
+
+RSpec.describe Pawn do
+  subject(:pawn) { described_class.new }
+  describe '#path?' do
+    context '' do
+      it '' do
+      end
+    end
+  end
+end
+
+# rubocop:enable Metrics/BlockLength
