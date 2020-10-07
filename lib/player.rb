87e16e46c355679bdd9b42e645171a7b29c31790
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-07 11:43:40 -0700

    Add #obtain_move and #move_in_right_format?

--- lib/player.rb
+++ lib/player.rb
@@ -2,4 +2,14 @@
 
 # player class
 class Player
+  def obtain_move
+    puts 'Please enter your move'
+    gets.chomp
+  end
+
+  def move_in_right_format?(move)
+    return false if move.length != 4
+
+    move =~ /[a-h][1-8][a-h][1-8]/ ? true : false
+  end
 end
