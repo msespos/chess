0570855956fc958dadbafb22ec90458ef21d03fc
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-07 10:17:03 -0700

    Add player_spec.rb

---
+++ spec/player_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require_relative '../lib/player.rb'
+
+RSpec.describe Player do
+  subject(:player) { described_class.new }
+  describe '' do
+    context '' do
+      it '' do
+      end
+    end
+  end
+end
+
+# rubocop:enable Metrics/BlockLength
