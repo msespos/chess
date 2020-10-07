5bf0a9727a28633e834efada116cf0f81326cf3a
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-07 14:25:27 -0700

    Add comments

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,7 @@
 
 # player class
 class Player
+  # get the player's move and checking it, prompting if invalid moves are entered
   def player_move
     move = obtain_move_from_player
     until move_in_right_format?(move)
@@ -11,17 +12,20 @@ class Player
     move
   end
 
+  # used by #player_move to get the player's move before checking it
   def obtain_move_from_player
     puts 'Please enter your move'
     gets.chomp
   end
 
+  # used by #player_move to check if the move is in algebraic notation (e.g. a1a3)
   def move_in_right_format?(move)
     return false if move.length != 4
 
     move =~ /[a-h][1-8][a-h][1-8]/ ? true : false
   end
 
+  # used by #player_move as the message for an invalid move
   def invalid_move_message
     'That is not a valid move! Please enter a valid move'
   end
