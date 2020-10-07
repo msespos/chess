1ee9764118da06745fe72ef224f7696649bb044a
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-07 11:44:00 -0700

    Add tests for #move_in_right_format?

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,9 +6,40 @@ require_relative '../lib/player.rb'
 
 RSpec.describe Player do
   subject(:player) { described_class.new }
-  describe '' do
-    context '' do
-      it '' do
+  describe '#move_in_right_format?' do
+    context 'it is a move in the right format' do
+      it 'returns true' do
+        right_format_or_not = player.move_in_right_format?('a1a3')
+        expect(right_format_or_not).to eq(true)
+      end
+
+      it 'returns true' do
+        right_format_or_not = player.move_in_right_format?('b3h4')
+        expect(right_format_or_not).to eq(true)
+      end
+    end
+
+    context 'it is a move of the wrong length' do
+      it 'returns false' do
+        right_format_or_not = player.move_in_right_format?('a1a3a5')
+        expect(right_format_or_not).to eq(false)
+      end
+
+      it 'returns false' do
+        right_format_or_not = player.move_in_right_format?('b3')
+        expect(right_format_or_not).to eq(false)
+      end
+    end
+
+    context 'it is a move in the wrong format' do
+      it 'returns false' do
+        right_format_or_not = player.move_in_right_format?('aabb')
+        expect(right_format_or_not).to eq(false)
+      end
+
+      it 'returns false' do
+        right_format_or_not = player.move_in_right_format?('b0h9')
+        expect(right_format_or_not).to eq(false)
       end
     end
   end
