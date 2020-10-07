0e636170cdaae1f00cbe849e9c02c63a35309802
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-07 14:21:32 -0700

    Add two tests for Player#player_move

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,6 +6,28 @@ require_relative '../lib/player.rb'
 
 RSpec.describe Player do
   subject(:player) { described_class.new }
+  describe '#player_move' do
+    context 'when a1a3 is input' do
+      before do
+        allow(player).to receive(:obtain_move_from_player).and_return('a1a3')
+      end
+      it 'returns a1a3' do
+        player_move_return = player.player_move
+        expect(player_move_return).to eq('a1a3')
+      end
+    end
+
+    context 'when input is invalid twice, then a1a3 is input' do
+      before do
+        allow(player).to receive(:obtain_move_from_player).and_return('invalid', 'invalid', 'a1a3')
+      end
+      it 'returns a1a3' do
+        player_move_return = player.player_move
+        expect(player_move_return).to eq('a1a3')
+      end
+    end
+  end
+
   describe '#move_in_right_format?' do
     context 'it is a move in the right format' do
       it 'returns true' do
