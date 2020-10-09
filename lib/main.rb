638941c52a9461e957f6f855dc296d0c2e430ebb
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-09 09:06:34 -0700

    Add require_relative for all other lib files to main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 require_relative 'game.rb'
+require_relative 'player.rb'
+require_relative 'board.rb'
+require_relative 'piece.rb'
+require_relative 'pawn.rb'
+require_relative 'knight.rb'
+require_relative 'rook.rb'
+require_relative 'bishop.rb'
+require_relative 'queen.rb'
+require_relative 'king.rb'
 
 Game.new.play
