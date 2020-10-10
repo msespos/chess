6b5455dabe084c3c029c8b4f2e1cd0de467ccaf6
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-10 14:52:13 -0700

    Rename #start_and_finish_pieces_valid? to #start_and_finish_spaces_valid? and edit comments

--- lib/game.rb
+++ lib/game.rb
@@ -76,7 +76,7 @@ class Game
   # used by #move_piece
   # check if the move is valid by checking if the start and finish squares are different
   # and checking if the start and finish squares are on the playing field
-  # and checking if the start and finish squares are the same color or the finish is not nil)
+  # and checking if the start and finish spaces are valid (same color, not nil)
   # and looking up the appropriate Piece path method using path_method_from_piece
   # and calling it on @piece
   def valid_move?(start, finish)
@@ -84,14 +84,22 @@ class Game
 
     return false unless on_playing_field?(start) && on_playing_field?(finish)
 
-    return false unless start_and_finish_pieces_valid?(start, finish)
+    return false unless start_and_finish_spaces_valid?(start, finish)
 
     start_piece = @playing_field[start[0]][start[1]]
     path_method = path_method_from_piece(start_piece)
     @piece.send(path_method, start, finish, @playing_field)
   end
 
-  def start_and_finish_pieces_valid?(start, finish)
+  # used by #valid_move? to check if a set of coordinates is on the board
+  def on_playing_field?(coordinates)
+    coordinates[0] >= 0 && coordinates[0] <= 7 && coordinates[1] >= 0 && coordinates[1] <= 7
+  end
+
+  # used by valid_move? to check that the start and finish spaces are valid
+  # check if start space is occupied
+  # and call #finish_space_valid? on the start and finish pieces
+  def start_and_finish_spaces_valid?(start, finish)
     start_piece = @playing_field[start[0]][start[1]]
     finish_piece = @playing_field[finish[0]][finish[1]]
     return false if start_piece.nil?
@@ -101,12 +109,7 @@ class Game
     true
   end
 
-  # used by #valid_move? to check if a set of coordinates is on the board
-  def on_playing_field?(coordinates)
-    coordinates[0] >= 0 && coordinates[0] <= 7 && coordinates[1] >= 0 && coordinates[1] <= 7
-  end
-
-  # used by #valid_move?
+  # used by start_and_finish_spaces_valid?
   # check if the piece in the finish square is nil or not
   # or if the pieces in the start and finish square are the same color or not
   def finish_space_valid?(start_piece, finish_piece)
