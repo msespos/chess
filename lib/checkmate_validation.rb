4b58673050b935e2e32ef6782b8d2df9806a9805
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-28 08:02:11 -0700

    Disable rubocop ModuleLength as module is only 6 lines over the limit

--- lib/checkmate_validation.rb
+++ lib/checkmate_validation.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ModuleLength
+
 # methods for testing for checkmate
 module CheckmateValidation
   # check if the king is in checkmate using helper methods
@@ -157,3 +159,5 @@ module CheckmateValidation
     squares
   end
 end
+
+# rubocop:enable Metrics/ModuleLength
