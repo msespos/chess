0e859db7b031a2a8fbe6ed7bc26692b10a70ed6a
Mike Esposito <mikeesposito6852@gmail.com>
2020-10-30 08:41:54 -0700

    Refactor #in_stalemate? into three separate smaller methods

--- lib/stalemate_validation.rb
+++ lib/stalemate_validation.rb
@@ -2,29 +2,44 @@
 
 # methods for testing for stalemate
 module StalemateValidation
+  # check if a player is in stalemate by checking if they have any valid moves
   def in_stalemate?
     return false if in_checkmate?
 
     # make a copy of the playing field to revert to after testing king moves
     playing_field_before_move = @playing_field.clone.map(&:clone)
+
+    return false unless no_valid_moves_on_board?(playing_field_before_move)
+
+    # revert to pre-test-move copy of playing field
+    @playing_field = playing_field_before_move
+    true
+  end
+
+  def no_valid_moves_on_board?(playing_field_before_move)
     @playing_field.each_with_index do |column, column_index|
       column.each_index do |row_index|
         piece = @playing_field[column_index][row_index]
         next if piece.nil? || piece[0] != @current_player[0]
 
-        @playing_field.each_with_index do |inner_column, inner_column_index|
-          inner_column.each_index do |inner_row_index|
-            next if move_piece([column_index, row_index], [inner_column_index, inner_row_index]) == :invalid
-
-            # revert to pre-test-move copy of playing field
-            @playing_field = playing_field_before_move
-            return false
-          end
-        end
+        return false unless no_valid_moves_for_piece?(playing_field_before_move,
+                                                      column_index, row_index)
       end
     end
-    # revert to pre-test-move copy of playing field
-    @playing_field = playing_field_before_move
+    true
+  end
+
+  def no_valid_moves_for_piece?(playing_field_before_move, column_index, row_index)
+    @playing_field.each_with_index do |inner_column, inner_column_index|
+      inner_column.each_index do |inner_row_index|
+        next if move_piece([column_index, row_index],
+                           [inner_column_index, inner_row_index]) == :invalid
+
+        # revert to pre-test-move copy of playing field
+        @playing_field = playing_field_before_move
+        return false
+      end
+    end
     true
   end
 end
