2faf2efb795db13f4c729648a3519b6350a9a65a
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-01 09:02:53 -0800

    Add tests for #add_captured_pieces

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -140,6 +140,74 @@ RSpec.describe Board do
       end
     end
   end
+
+  describe '#add_captured_pieces' do
+    context 'when it receives a set of captured pieces from Game in the beginning of a game' do
+      it 'prints out the board and captured pieces' do
+        board.overwrite_playing_field([[:w_rook, :w_pawn, nil, nil, nil, nil, :b_pawn, :b_rook],
+                                       [:w_knight, :w_pawn, nil, nil, nil, nil, :b_pawn, :b_knight],
+                                       [nil, :w_pawn, nil, nil, nil, nil, :b_pawn, :b_bishop],
+                                       [nil, :w_queen, nil, nil, nil, :b_pawn, nil, nil],
+                                       [:w_king, :w_pawn, nil, nil, :b_pawn, nil, nil, :b_king],
+                                       [:w_bishop, :w_pawn, nil, nil, nil, nil, nil, nil],
+                                       [nil, :w_pawn, nil, nil, nil, nil, :b_pawn, :b_knight],
+                                       [:w_rook, :w_pawn, nil, nil, nil, nil, :b_pawn, :w_knight]])
+        board.add_captured_pieces([[:b_rook, :b_bishop, :b_queen, :b_pawn, nil, nil, nil, nil],
+                                   [nil, nil, nil, nil, nil, nil, nil, nil],
+                                   [:w_bishop, :w_pawn, nil, nil, nil, nil, nil, nil],
+                                   [nil, nil, nil, nil, nil, nil, nil, nil]])
+        expect { puts(board) }.to output(<<-BOARD).to_stdout
+
+     a b c d e f g h
+
+ 8   ♜ ♞ ♝ - ♚ - ♞ ♘   8   ♗ ♙
+ 7   ♟ ♟ ♟ - - - ♟ ♟   7  
+ 6   - - - ♟ - - - -   6  
+ 5   - - - - ♟ - - -   5  
+ 4   - - - - - - - -   4  
+ 3   - - - - - - - -   3  
+ 2   ♙ ♙ ♙ ♕ ♙ ♙ ♙ ♙   2  
+ 1   ♖ ♘ - - ♔ ♗ - ♖   1   ♜ ♝ ♛ ♟
+
+     a b c d e f g h
+
+        BOARD
+      end
+    end
+
+    context 'when it receives a set of captured pieces from Game in endgame' do
+      xit 'prints out the board and captured pieces' do
+        board.overwrite_playing_field([[nil, :w_pawn, nil, nil, nil, nil, :b_pawn, nil],
+                                       [nil, nil, nil, :w_pawn, nil, nil, :b_bishop, nil],
+                                       [:w_rook, nil, nil, :b_pawn, nil, nil, nil, nil],
+                                       [nil, nil, nil, nil, nil, nil, nil, nil],
+                                       [nil, nil, nil, nil, nil, nil, nil, nil],
+                                       [nil, :w_king, nil, nil, nil, nil, nil, nil],
+                                       [nil, nil, :w_pawn, :b_king, nil, nil, nil, nil],
+                                       [nil, nil, nil, :w_pawn, :b_pawn, nil, nil, nil]])
+        board.add_captured_pieces([%i[b_knight b_bishop b_rook b_pawn b_knight b_queen b_pawn b_pawn],
+                                   [:b_rook, :b_pawn, :b_pawn, nil, nil, nil, nil, nil],
+                                   %i[w_rook w_queen w_pawn w_pawn w_knight w_knight w_bishop w_pawn],
+                                   [:w_pawn, :w_bishop, nil, nil, nil, nil, nil, nil]])
+        expect { puts(board) }.to output(<<-BOARD).to_stdout
+
+     a b c d e f g h
+
+ 8   - - - - - - - -   8   ♖ ♕ ♙ ♙ ♘ ♘ ♗ ♙
+ 7   ♟ ♝ - - - - - -   7   ♙ ♗
+ 6   - - - - - - - -   6  
+ 5   - - - - - - - ♟   5  
+ 4   - ♙ ♟ - - - ♚ ♙   4  
+ 3   - - - - - - ♙ -   3  
+ 2   ♙ - - - - ♔ - -   2   ♜ ♟ ♟
+ 1   - - ♖ - - - - -   1   ♞ ♝ ♜ ♟ ♞ ♛ ♟ ♟
+
+     a b c d e f g h
+
+        BOARD
+      end
+    end
+  end
 end
 
 # rubocop:enable Metrics/BlockLength
