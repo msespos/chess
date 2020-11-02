eae82a1de7eef1a324b77e2edfa54391767d6053
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-02 15:16:42 -0800

    Rename #pawn_to_promote? to #pawn_to_promote and remove one of the returns

--- lib/pawn_promotion.rb
+++ lib/pawn_promotion.rb
@@ -2,12 +2,18 @@
 
 # methods for handling pawn promotion
 module PawnPromotion
-  def pawn_to_promote?
+  def pawn_to_promote
     (0..7).each do |column|
-      return [:black, [column, 0]] if @playing_field[column][0] == :b_pawn
+      return [:black, column] if @playing_field[column][0] == :b_pawn
 
-      return [:white, [column, 7]] if @playing_field[column][7] == :w_pawn
+      return [:white, column] if @playing_field[column][7] == :w_pawn
     end
     false
+  end
+
+  def promote_pawn
+    color, column = pawn_to_promote
+    new_piece = @player.obtain_promotion_piece_choice
+    color == :black ? @playing_field[column][0] = new_piece : @playing_field[column][7] = new_piece
   end
 end
