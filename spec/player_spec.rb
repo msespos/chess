677aad144ca557df63ae3d4783af6a6103c58009
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-03 12:47:39 -0800

    Rewrite tests to reflect change in method names and arguments

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,64 +6,64 @@ require_relative '../lib/player.rb'
 
 RSpec.describe Player do
   subject(:player) { described_class.new }
-  describe '#player_move' do
+  describe '#user_input' do
     context 'when a1a3 is input' do
       before do
-        allow(player).to receive(:obtain_player_move).and_return('a1a3')
+        allow(player).to receive(:obtain_user_input).and_return('a1a3')
       end
 
       it 'returns a1a3' do
-        player_move_return = player.player_move
-        expect(player_move_return).to eq('a1a3')
+        input = player.user_input(:move)
+        expect(input).to eq('a1a3')
       end
     end
 
     context 'when input is invalid twice, then a1a3 is input' do
       before do
-        allow(player).to receive(:obtain_player_move).and_return('invalid', 'invalid', 'a1a3')
+        allow(player).to receive(:obtain_user_input).and_return('invalid', 'invalid', 'a1a3')
         allow(player).to receive(:puts)
       end
 
       it 'returns a1a3' do
-        player_move_return = player.player_move
-        expect(player_move_return).to eq('a1a3')
+        input = player.user_input(:move)
+        expect(input).to eq('a1a3')
       end
     end
   end
 
-  describe '#move_in_right_format?' do
-    context 'it is a move in the right format' do
+  describe '#input_in_right_format?' do
+    context 'it is a input in the right format' do
       it 'returns true' do
-        right_format_or_not = player.move_in_right_format?('a1a3')
+        right_format_or_not = player.input_in_right_format?('a1a3', :move)
         expect(right_format_or_not).to eq(true)
       end
 
       it 'returns true' do
-        right_format_or_not = player.move_in_right_format?('b3h4')
+        right_format_or_not = player.input_in_right_format?('b3h4', :move)
         expect(right_format_or_not).to eq(true)
       end
     end
 
-    context 'it is a move of the wrong length' do
+    context 'it is a input of the wrong length' do
       it 'returns false' do
-        right_format_or_not = player.move_in_right_format?('a1a3a5')
+        right_format_or_not = player.input_in_right_format?('a1a3a5', :move)
         expect(right_format_or_not).to eq(false)
       end
 
       it 'returns false' do
-        right_format_or_not = player.move_in_right_format?('b3')
+        right_format_or_not = player.input_in_right_format?('b3', :move)
         expect(right_format_or_not).to eq(false)
       end
     end
 
-    context 'it is a move in the wrong format' do
+    context 'it is a input in the wrong format' do
       it 'returns false' do
-        right_format_or_not = player.move_in_right_format?('aabb')
+        right_format_or_not = player.input_in_right_format?('aabb', :move)
         expect(right_format_or_not).to eq(false)
       end
 
       it 'returns false' do
-        right_format_or_not = player.move_in_right_format?('b0h9')
+        right_format_or_not = player.input_in_right_format?('b0h9', :move)
         expect(right_format_or_not).to eq(false)
       end
     end
