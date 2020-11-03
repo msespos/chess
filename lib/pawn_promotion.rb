be3575f8ef4046c11a13040fc371e14261bb7098
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-03 13:33:12 -0800

    Add INPUT_TO_PIECE hash for converting single character inputs to strings and add #input_to_piece and call to #input_to_piece in #promote_pawn for converting user input to a piece symbol

--- lib/pawn_promotion.rb
+++ lib/pawn_promotion.rb
@@ -2,6 +2,11 @@
 
 # methods for handling pawn promotion
 module PawnPromotion
+  INPUT_TO_PIECE = { n: 'knight',
+                     b: 'bishop',
+                     r: 'rook',
+                     q: 'queen' }.freeze
+
   def pawn_to_promote
     (0..7).each do |column|
       return [:black, column] if @playing_field[column][0] == :b_pawn
@@ -13,7 +18,14 @@ module PawnPromotion
 
   def promote_pawn
     color, column = pawn_to_promote
-    new_piece = @player.obtain_promotion_piece_choice
+    input = @player.user_input(:piece)
+    new_piece = input_to_piece(input, color)
     color == :black ? @playing_field[column][0] = new_piece : @playing_field[column][7] = new_piece
+  end
+
+  def input_to_piece(input, color)
+    input = input.downcase.to_sym
+    new_piece_without_color = INPUT_TO_PIECE[input]
+    color == :black ? ('b_' + new_piece_without_color).to_sym : ('w_' + new_piece_without_color).to_sym
   end
 end
