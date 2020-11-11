7209c7e5a834535b0713e6e3f35761b329e6240b
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-11 08:52:14 -0800

    Check finish piece instead of start piece to correct timing of check

--- lib/en_passant.rb
+++ lib/en_passant.rb
@@ -5,10 +5,10 @@ module EnPassant
   # check to see if a pawn has just moved two squares and in which column
   # if so, set @en_passant_column to that column
   def check_for_en_passant(start, finish)
-    start_piece = @playing_field[start[0]][start[1]]
-    if start_piece == :w_pawn && start[1] == 1 && finish[1] == 3
+    finish_piece = @playing_field[finish[0]][finish[1]]
+    if finish_piece == :w_pawn && start[1] == 1 && finish[1] == 3
       @en_passant_column = start[0]
-    elsif start_piece == :b_pawn && start[1] == 6 && finish[1] == 4
+    elsif finish_piece == :b_pawn && start[1] == 6 && finish[1] == 4
       @en_passant_column = start[0]
     else
       @en_passant_column = nil
