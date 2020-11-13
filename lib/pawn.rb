9605c5b64a9bb9e1e6d1acd434883563e4b85f9e
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-13 11:20:43 -0800

    Refactor #one_square_ahead_free, #left_diagonal_capture, #right_diagonal_capture, #left_en_passant, and #right_en_passant for conciseness and remove unnecessary helper methods

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/ClassLength
-
 # pawn class
 class Pawn
   # determine if a standard path is legal for a pawn using the start, finish, playing field and color
@@ -63,64 +61,25 @@ class Pawn
   # used by #standard_conditions_met? to determine if the square in front of a
   # starting position pawn is free
   def one_square_ahead_free?(start, finish, playing_field, color)
-    if color == :white
-      white_one_square?(start, finish, playing_field)
-    else
-      black_one_square?(start, finish, playing_field)
-    end
-  end
-
-  # used by one_square_ahead_free? to determine if the one square ahead
-  # of a white pawn is free
-  def white_one_square?(start, finish, playing_field)
-    finish[0] == start[0] && finish[1] == start[1] + 1 && playing_field[finish[0]][finish[1]].nil?
-  end
-
-  # used by one_square_ahead_free? to determine if the one square ahead
-  # of a black pawn is free
-  def black_one_square?(start, finish, playing_field)
-    finish[0] == start[0] && finish[1] == start[1] - 1 && playing_field[finish[0]][finish[1]].nil?
+    move_ahead = color == :white ? 1 : - 1
+    finish[0] == start[0] &&
+      finish[1] == start[1] + move_ahead &&
+      playing_field[finish[0]][finish[1]].nil?
   end
 
   # used by #standard_conditions_met? to determine if a left diagonal capture is possible
   def left_diagonal_capture?(start, finish, playing_field, color)
-    if color == :white
-      white_left_diagonal?(start, finish, playing_field)
-    else
-      black_left_diagonal?(start, finish, playing_field)
-    end
-  end
-
-  # used by #left_diagonal_capture to check the white pawn case
-  def white_left_diagonal?(start, finish, playing_field)
-    finish[0] == start[0] - 1 && finish[1] == start[1] + 1 &&
-      !square_empty?(finish, playing_field)
-  end
-
-  # used by #left_diagonal_capture to check the black pawn case
-  def black_left_diagonal?(start, finish, playing_field)
-    finish[0] == start[0] + 1 && finish[1] == start[1] - 1 &&
+    move_diagonal = color == :white ? 1 : -1
+    finish[0] == start[0] - move_diagonal &&
+      finish[1] == start[1] + move_diagonal &&
       !square_empty?(finish, playing_field)
   end
 
   # used by #standard_conditions_met? to determine if a right diagonal capture is possible
   def right_diagonal_capture?(start, finish, playing_field, color)
-    if color == :white
-      white_right_diagonal?(start, finish, playing_field)
-    else
-      black_right_diagonal?(start, finish, playing_field)
-    end
-  end
-
-  # used by #right_diagonal_capture to check the white pawn case
-  def white_right_diagonal?(start, finish, playing_field)
-    finish[0] == start[0] + 1 && finish[1] == start[1] + 1 &&
-      !square_empty?(finish, playing_field)
-  end
-
-  # used by #right_diagonal_capture to check the black pawn case
-  def black_right_diagonal?(start, finish, playing_field)
-    finish[0] == start[0] - 1 && finish[1] == start[1] - 1 &&
+    move_diagonal = color == :white ? 1 : -1
+    finish[0] == start[0] + move_diagonal &&
+      finish[1] == start[1] + move_diagonal &&
       !square_empty?(finish, playing_field)
   end
 
@@ -149,49 +108,19 @@ class Pawn
 
   # used by #en_passant_conditions_met? to check for a left en passant
   def left_en_passant?(start, finish, playing_field, color, en_passant_column)
-    if color == :white
-      left_white_en_passant(start, finish, playing_field, en_passant_column)
-    else
-      left_black_en_passant(start, finish, playing_field, en_passant_column)
-    end
-  end
-
-  # used by #left_en_passant? to check for the white pawn case
-  def left_white_en_passant(start, finish, playing_field, en_passant_column)
-    finish[0] == start[0] - 1 && finish[1] == start[1] + 1 &&
+    move_en_passant = color == :white ? 1 : -1
+    finish[0] == start[0] - move_en_passant &&
+      finish[1] == start[1] + move_en_passant &&
       square_empty?(finish, playing_field) &&
-      en_passant_column == start[0] - 1
-  end
-
-  # used by #left_en_passant? to check for the black pawn case
-  def left_black_en_passant(start, finish, playing_field, en_passant_column)
-    finish[0] == start[0] + 1 && finish[1] == start[1] - 1 &&
-      square_empty?(finish, playing_field) &&
-      en_passant_column == start[0] + 1
+      en_passant_column == start[0] - move_en_passant
   end
 
   # used by #en_passant_conditions_met? to check for a right en passant
   def right_en_passant?(start, finish, playing_field, color, en_passant_column)
-    if color == :white
-      right_white_en_passant(start, finish, playing_field, en_passant_column)
-    else
-      right_black_en_passant(start, finish, playing_field, en_passant_column)
-    end
-  end
-
-  # used by #right_en_passant? to check for the white pawn case
-  def right_white_en_passant(start, finish, playing_field, en_passant_column)
-    finish[0] == start[0] + 1 && finish[1] == start[1] + 1 &&
+    move_en_passant = color == :white ? 1 : -1
+    finish[0] == start[0] + move_en_passant &&
+      finish[1] == start[1] + move_en_passant &&
       square_empty?(finish, playing_field) &&
-      en_passant_column == start[0] + 1
-  end
-
-  # used by #right_en_passant? to check for the black pawn case
-  def right_black_en_passant(start, finish, playing_field, en_passant_column)
-    finish[0] == start[0] - 1 && finish[1] == start[1] - 1 &&
-      square_empty?(finish, playing_field) &&
-      en_passant_column == start[0] - 1
+      en_passant_column == start[0] + move_en_passant
   end
 end
-
-# rubocop:enable Metrics/ClassLength
