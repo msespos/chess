253afd60c106d115cee5a062c152e20de6a9daf5
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-20 10:45:14 -0800

    Add one_player_version.rb and OnePlayerVersion module and #play_computer_turn and #random_square to implement a one player version of the game in which the computer plays randomly

---
+++ lib/one_player_version.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# methods for the one player version of the game
+module OnePlayerVersion
+
+  def play_computer_turn
+    start = random_square
+    finish = random_square
+    while move_piece(start, finish, false, true) == invalid
+      start = random_square
+      finish = random_square
+    end
+  end
+
+  def random_square
+    [rand(8), rand(8)]
+  end
+end
