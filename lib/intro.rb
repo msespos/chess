87f9c7374ea3048762c2e99f61680fdffb78e1b4
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-30 09:34:07 -0800

    Add #intro_text with filler text and #leave_intro to prompt the player to start the game

--- lib/intro.rb
+++ lib/intro.rb
@@ -2,4 +2,15 @@
 
 # text for game intro
 module Intro
+
+  def intro_text
+    puts "\e[H\e[2J"
+    "Intro\n"
+  end
+
+  def leave_intro
+    print "Press any key to start the game!"
+    STDIN.getch # gets the input
+    puts "\e[H\e[2J" # clears the screen
+  end
 end
