59af46e03c4f80660f102bde61d33b5780536f40
Mike Esposito <mikeesposito6852@gmail.com>
2020-11-30 09:37:54 -0800

    Style fixes

--- lib/intro.rb
+++ lib/intro.rb
@@ -2,14 +2,13 @@
 
 # text for game intro
 module Intro
-
   def intro_text
     puts "\e[H\e[2J"
     "Intro\n"
   end
 
   def leave_intro
-    print "Press any key to start the game!"
+    print 'Press any key to start the game!'
     STDIN.getch # gets the input
     puts "\e[H\e[2J" # clears the screen
   end
